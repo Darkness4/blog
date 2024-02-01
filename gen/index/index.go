1b4524f92c51cf2b0e9c65b021328eb779c51d7b
Nguyen Marc <nguyen_marc@live.fr>
2024-02-01 10:51:49 +0100

    fix(sitemap): fix loc

--- gen/index/index.go
+++ gen/index/index.go
@@ -10,8 +10,9 @@ type Index struct {
 	Title         string    `xml:"-"`
 	Description   string    `xml:"-"`
 	PublishedDate time.Time `xml:"lastmod"`
-	Href          string    `xml:"loc"`
+	Href          string    `xml:"-"`
 	EntryName     string    `xml:"-"`
+	Loc           string    `xml:"loc"`
 	Priority      float32   `xml:"priority,omitempty"`
 }
 
@@ -25,6 +26,7 @@ var Pages = [][]Index{
 			Description:   "Developing a simple WebAuthn authentication service in Go, as there are few functional implementations of WebAuthn with Go, and only a few existing guides.",
 			PublishedDate: time.Unix(1706313600, 0),
 			Href:          "/blog/2024-01-27-webauthn-guide",
+			Loc:           "https://blog.mnguyen.fr/blog/2024-01-27-webauthn-guide",
 			Priority:      0.5,
 		},
 		{
@@ -33,6 +35,7 @@ var Pages = [][]Index{
 			Description:   "Simple guide and recommendations about CGO. For documentation purposes.",
 			PublishedDate: time.Unix(1704931200, 0),
 			Href:          "/blog/2024-01-11-cgo-guide",
+			Loc:           "https://blog.mnguyen.fr/blog/2024-01-11-cgo-guide",
 			Priority:      0.5,
 		},
 		{
@@ -41,6 +44,7 @@ var Pages = [][]Index{
 			Description:   "Have you ever wondered whether learning the wrong software architecture is really \"wrong\"? Personally, I've always asked myself this question, and more often than not I've found my answer on the job.",
 			PublishedDate: time.Unix(1703721600, 0),
 			Href:          "/blog/2023-12-28-architecture-paradigms",
+			Loc:           "https://blog.mnguyen.fr/blog/2023-12-28-architecture-paradigms",
 			Priority:      0.5,
 		},
 		{
@@ -49,6 +53,7 @@ var Pages = [][]Index{
 			Description:   "The review about Gentoo Linux after 1 year of intensive usage in gaming and development: it's the best OS in the world.",
 			PublishedDate: time.Unix(1702512000, 0),
 			Href:          "/blog/2023-12-14-about-gentoo-linux",
+			Loc:           "https://blog.mnguyen.fr/blog/2023-12-14-about-gentoo-linux",
 			Priority:      0.5,
 		},
 		{
@@ -57,6 +62,7 @@ var Pages = [][]Index{
 			Description:   "Want to statically compile for multi-platform in Go super-easily? Let me introduce Portage, Gentoo's package manager, and Crossdev, Gentoo's solution for cross-compilation.",
 			PublishedDate: time.Unix(1699401600, 0),
 			Href:          "/blog/2023-11-08-go-with-portage-and-crossdev",
+			Loc:           "https://blog.mnguyen.fr/blog/2023-11-08-go-with-portage-and-crossdev",
 			Priority:      0.5,
 		},
 		{
@@ -65,6 +71,7 @@ var Pages = [][]Index{
 			Description:   "A rant about people implementing their own user database. Also, a guide with detailed implementations on OAuth2/OIDC.",
 			PublishedDate: time.Unix(1696809600, 0),
 			Href:          "/blog/2023-10-09-understanding-authentication",
+			Loc:           "https://blog.mnguyen.fr/blog/2023-10-09-understanding-authentication",
 			Priority:      0.5,
 		},
 		{
@@ -73,6 +80,7 @@ var Pages = [][]Index{
 			Description:   "About learning your first programming language in 2023. Yes, it's a filler post.",
 			PublishedDate: time.Unix(1695340800, 0),
 			Href:          "/blog/2023-09-22-learn-programming-language",
+			Loc:           "https://blog.mnguyen.fr/blog/2023-09-22-learn-programming-language",
 			Priority:      0.5,
 		},
 		{
@@ -81,6 +89,7 @@ var Pages = [][]Index{
 			Description:   "About replicable infrastructure when containerization and virtualization are not allowed.",
 			PublishedDate: time.Unix(1694822400, 0),
 			Href:          "/blog/2023-09-16-road-to-replicable-infrastructure",
+			Loc:           "https://blog.mnguyen.fr/blog/2023-09-16-road-to-replicable-infrastructure",
 			Priority:      0.5,
 		},
 		{
@@ -89,6 +98,7 @@ var Pages = [][]Index{
 			Description:   "This article documents about how this blog came to be. From technical choices to deploying this blog.",
 			PublishedDate: time.Unix(1694304000, 0),
 			Href:          "/blog/2023-09-10-developing-blog",
+			Loc:           "https://blog.mnguyen.fr/blog/2023-09-10-developing-blog",
 			Priority:      0.5,
 		},
 		{
@@ -97,6 +107,7 @@ var Pages = [][]Index{
 			Description:   "The very first article. About the motivations of developing this blog from scratch with Go and HTMX, and why I want to write articles on this blog.",
 			PublishedDate: time.Unix(1694217600, 0),
 			Href:          "/blog/2023-09-09-hello-world",
+			Loc:           "https://blog.mnguyen.fr/blog/2023-09-09-hello-world",
 			Priority:      0.5,
 		},
 	},
@@ -112,7 +123,7 @@ func ToSiteMap(ii [][]Index) ([]byte, error) {
 				Title:         "Marc Nguyen's Blog",
 				Description:   "Marc Nguyen's blog is a personal and technical blog about documenting some processes, implementations, etc.",
 				PublishedDate: time.Now(),
-				Href:          "https://blog.mnguyen.fr",
+				Loc:           "https://blog.mnguyen.fr",
 				Priority:      0.8,
 			},
 		},
@@ -136,7 +147,7 @@ var Feed = &feeds.Feed{
 		Email: "nguyen_marc@live.fr",
 	},
 	Created: time.Unix(1694131200, 0),
-	Updated: time.Unix(1706656793, 0),
+	Updated: time.Unix(1706781068, 0),
 	Items: []*feeds.Item{
 		{
 			Title:       "A guide to WebAuthn.",
